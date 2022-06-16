2779a85e38a0a1f736b11846dd8db692e8208e0e
Juan Roldán <juannroldan340@gmail.com>
2022-06-16 17:16:47 -0300

    UnitOfWork and Generic Repository added.

---
+++ OngProject/DataAccess/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace OngProject.DataAccess
+{
+    public class UnitOfWork
+    {
+        private readonly DbContext _context;
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ OngProject/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Repositories.Interfaces
+{
+    interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> Insert(T entity);
+        List<Task<T>> GetAll();
+        Task<T> GetById(int Id);
+        Task<T> Delete(int Id);
+        Task<T> Update(T entity);
+    }
+}

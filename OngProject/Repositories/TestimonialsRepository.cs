a340cafb52b9f2f9bb272827bc1ddb712a1ee02e
Juan Roldán <juannroldan340@gmail.com>
2022-06-17 15:29:44 -0300

    Requested changes applied.

--- OngProject/Repositories/TestimonialsRepository.cs
+++ OngProject/Repositories/TestimonialsRepository.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using OngProject.DataAccess;
 using OngProject.Repositories.Interfaces;
 
 namespace OngProject.Repositories
 {
-    public class TestimonialsRepository : GenericRepository<Testimonials>
+    public class TestimonialsRepository : GenericRepository<Testimonials>, ITestimonialsRepository
     {
         public TestimonialsRepository(DbContext context) : base(context)
         {

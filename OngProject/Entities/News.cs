620355b46bb40d66878e907ea7d9b171e1670e0d
Juan Roldán <juannroldan340@gmail.com>
2022-06-17 15:21:14 -0300

    News entity fixed.

--- OngProject/Entities/News.cs
+++ OngProject/Entities/News.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,6 +18,7 @@ namespace OngProject.Entities
         [StringLength(255)]
         public string Image { get; set; }
 
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
     }
 }

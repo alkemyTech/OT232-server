ada2f5f12299fe278f874cd6c4f1a21cc707e5f5
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-06-20 11:57:09 -0300

    Activities

---
+++ OngProject/Controllers/ActivitiesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        private IActionResult Ok()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int Id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Created("", null);
+        }
+
+        private IActionResult Created(string v, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Created("", null);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ OngProject/Repositories/ActivitiesRepository.cs
@@ -0,0 +1,15 @@
+﻿using OngProject.DataAccess;
+
+
+namespace OngProject.Repositories
+{
+    public class ActivitiesRepository : GenericRepository<Activity>
+    {
+        private readonly DbContext _context;
+
+        public ActivitiesRepository(DbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}

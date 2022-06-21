c030c2da3b1ef221cd61e01fb5ad016863bed991
santitiribelli <santi_tiri@hotmail.com>
2022-06-21 16:37:37 -0300

    Fix carpeta

---
+++ OngProject/Entities/Activitie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OngProject.Entities
+{
+    public class Activitie : BaseEntity
+    {
+        
+        [Required,StringLength(255)]
+        public string name { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public string content { get; set; }
+
+        [Required,StringLength(255)]
+        public string image { get; set; }
+
+    }
+}

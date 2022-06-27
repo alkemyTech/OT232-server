09a17560de1514330e8f6a7b96189bb30dbcbb07
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-06-26 21:22:17 -0300

    seeds Category

---
+++ OngProject/DataAccess/CategoriesSeeds.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OngProject.Entities;
+
+namespace OngProject.DataAccess
+{
+    public static class CategoriesSeeds
+    {
+        
+        public static void CategoriesSeed(this ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Category>().HasData(
+            new Category
+            {
+                Id = 1,
+                Name = "Programas Educativos",
+                Description = " Buscamos incrementar la capacidad intlectual, moral y efectiva de las personas de acuerdo a la culturay las nor,as de convivencia a la que pertenecen ",
+                Image = "Somosmas.jpg",
+
+            });
+            
+            
+
+        }
+    }
+
+    
+}

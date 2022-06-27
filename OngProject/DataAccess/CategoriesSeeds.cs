a279df26a834063ab8c85fbb18a97afcb60d3b64
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-06-27 10:24:55 -0300

    modification

--- OngProject/DataAccess/CategoriesSeeds.cs
+++ OngProject/DataAccess/CategoriesSeeds.cs
@@ -17,9 +17,46 @@ namespace OngProject.DataAccess
                 Description = " Buscamos incrementar la capacidad intlectual, moral y efectiva de las personas de acuerdo a la culturay las nor,as de convivencia a la que pertenecen ",
                 Image = "Somosmas.jpg",
 
+            },
+            new Category
+            {
+                Id = 1,
+                Name = "Programas Educativos",
+                Description = " Buscamos incrementar la capacidad intlectual, moral y efectiva de las personas de acuerdo a la culturay las nor,as de convivencia a la que pertenecen ",
+                Image = "Somosmas.jpg",
+
+            },
+            new Category
+            {
+                Id = 1,
+                Name = "Programas Educativos Adolescentes",
+                Description = " Buscamos incrementar la capacidad efectiva y de vinculos con los pares para " +
+                "una mejor autonomia y trabjo grupal",
+                Image = "Somosmas.jpg",
+
+            },
+            new Category
+            {
+                Id = 1,
+                Name = "Programas Educativos Jovenes",
+                Description = " se pretnde crecimiento de habilidades, capacidades y nuevas herramientas para que logren un desempeño " +
+                "con mayor eficiencia ",
+                Image = "Somosmas.jpg",
+
+            },
+            new Category
+            {
+                Id = 1,
+                Name = "Programas Educativos Adulto",
+                Description = " Buscamos incrementar la capacidad y habilidades para un mejor desempeño dentro de la sociedad" +
+                "y que logren mas productividad ",
+                Image = "Somosmas.jpg",
+
             });
-            
-            
+
+
+
+
 
         }
     }

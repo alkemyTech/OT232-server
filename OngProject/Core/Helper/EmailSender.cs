e24907b50e7ce9ef322432745f974e51bb799eaf
Patricio <patodotaop@hotmail.com>
2022-06-28 13:59:37 -0300

    Creating EmailSender Service and Creating his SendEmail Method which receive an email adress.

---
+++ OngProject/Core/Helper/EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Helper
+{
+    public class EmailSender
+    {
+        IConfiguration _config;
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmail(string toEmail)
+        {
+            var apiKey = _config["SendGridToken"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("SampleMail", "SampleTitle");
+            var subject = "SampleSubject";
+            var to = new EmailAddress("SampleMail");
+            var plainTextContent = "sampleText";
+            var htmlContent = File.ReadAllText("./Templates/htmlpage.html");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            await client.SendEmailAsync(msg);
+        }
+    }
+}

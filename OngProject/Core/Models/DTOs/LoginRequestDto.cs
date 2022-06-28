d4051e53a808a4fa4b433b12234ccf74bf9631a5
Juan Roldán <juannroldan340@gmail.com>
2022-06-28 17:02:47 -0300

    DTOs Added

---
+++ OngProject/Core/Models/DTOs/LoginRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class LoginRequestDto
+    {
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener un mínimo de 6 (seis) caracteres.")]
+        public string Password { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+    }
+}

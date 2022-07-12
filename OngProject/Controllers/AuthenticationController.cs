89170ad5eb0dbefb926202ca5904da71314b2d2d
santiago <santiago.tiribelli@newsan.com.ar>
2022-07-12 13:21:35 -0300

    us -440

--- OngProject/Controllers/AuthenticationController.cs
+++ OngProject/Controllers/AuthenticationController.cs
@@ -21,7 +21,26 @@ namespace OngProject.Controllers
             _authenticationBusiness = authenticationBusiness;
             _emailSender = emailSender;
         }
-
+        // POST: /Register
+        /// <summary>
+        /// Crea un Usuario en la BD.
+        /// </summary>
+        /// <remarks>
+        /// Crea un nuevo usuario con los datos solicitados.
+        /// 
+        /// Sample Request:
+        /// 
+        ///                     FirstName = Nombre del usuario
+        ///                     LastName = Apellido del usuario
+        ///                     Email = Email del usuario
+        ///                     Password = Password que se va a utilizar
+        ///                     RoleID = El rol por defecto es 2.
+        /// </remarks>
+       
+        /// <response code="200">Ok. Objeto correctamente creado en la BD.</response>        
+        /// <response code="400">BadRequest. No se ha creado el objeto en la BD. Formato del objeto incorrecto.</response>
+        /// <response code="404">NotFound. No se ha encontrado el objeto solicitado.</response> 
+        /// <response code="500">InternalServerError, Error del servidor</response>
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterRequestDto request)
         {
@@ -39,6 +58,26 @@ namespace OngProject.Controllers
 
             return Ok(await _authenticationBusiness.GetToken(UserMapper.ToLoginUser(request)));
         }
+        /// <summary>
+        /// Autentifica la informacion de los usuarios registrados.
+        /// </summary>
+        /// <returns>retorna el JwtToken y un status Message</returns>
+        /// <remarks>
+        /// La informacion solicitada es
+        /// 
+        /// Sample request:
+        /// 
+        ///     POST / LOGIN
+        ///     {
+        ///         "email": "User@email.com",  *Required
+        ///         "password": "ExamplePassword"  *Required
+        ///     }
+        /// 
+        /// </remarks>
+        /// <response code="200">Retorna el JWT.</response>        
+        /// <response code="400">BadRequest. Formato del objeto incorrecto.</response>
+        /// <response code="404">NotFound. No se ha encontrado el objeto solicitado.</response> 
+        /// <response code="500">InternalServerError, Error del servidor</response>
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginUserDto user)
